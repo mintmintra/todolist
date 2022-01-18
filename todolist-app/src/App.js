412040bed70a9182bf81d3ae1fc584e9ff7234e0
mintmintra <mintracss019@gmail.com>
2022-01-19 00:35:49 +0700

    create form

--- todolist-app/src/App.js
+++ todolist-app/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container">
+      <h1>TodoList App</h1>
+      <form className="form-group">
+        <input type="text" className="text-input" />
+        <button type="submit" className="submit-btn">เพิ่มข้อมูล</button>
+      </form>
+    </section>
   );
 }
 
