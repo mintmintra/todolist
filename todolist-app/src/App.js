152396389b9184585186c2a1297843d02a0c45a3
mintmintra <mintracss019@gmail.com>
2022-01-20 23:21:40 +0700

    create State

--- todolist-app/src/App.js
+++ todolist-app/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("")
+  const submitData = (e) => {
+    e.preventDefault()
+    console.log("ข้อมูลรายการ = ", name)
+  }
   return (
     <section className="container">
       <h1>TodoList App</h1>
-      <form className="form-group">
-        <input type="text" className="text-input" />
-        <button type="submit" className="submit-btn">เพิ่มข้อมูล</button>
+      <form className="form-group" onSubmit={submitData}>
+        <div className="form-control">
+          <input type="text" className="text-input" onChange={(e)=>setName(e.target.value)} value={name} />
+          <button type="submit" className="submit-btn">เพิ่มข้อมูล</button>
+        </div>
       </form>
     </section>
   );
